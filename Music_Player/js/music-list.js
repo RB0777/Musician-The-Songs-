5f15b04100bea1aab8b9c5fac3442b9d0c3e9803
RB0777 <98003943+RB0777@users.noreply.github.com>
2022-07-29 02:03:43 +0530

    Add files via upload

---
+++ Music_Player/js/music-list.js
@@ -0,0 +1,21 @@
+let allMusic = [
+    {
+      name: "Young-Dumb-Broke",
+      artist: "Khalid",
+      img: "Young_Dumb",
+      src: "Young_Dumb"
+    },
+    {
+      name: "Baby",
+      artist: "Justin Beiber",
+      img: "Baby",
+      src: "Baby"
+    },
+    {
+      name: "Despacito",
+      artist: "	Luis Fonsi, Daddy Yankee",
+      img: "Despacito",
+      src: "Despacito"
+    },
+    
+  ];
